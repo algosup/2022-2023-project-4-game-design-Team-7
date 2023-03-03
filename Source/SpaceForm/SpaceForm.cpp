35e5ea5f031b1af66063f37e3f68c802558817a8
TheoDct <theo.diancourt@aglsoup.com>
2023-03-03 15:36:46 +0100

    init unreal project

---
+++ Source/SpaceForm/SpaceForm.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceForm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceForm, "SpaceForm" );
